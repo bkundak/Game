942625f33a4fa1d88850d210081ae9cd34e2eca7
obk <68072031+obk@users.noreply.github.com>
2020-09-07 12:26:27 +0300

    shop and main menu

--- hero.h
+++ hero.h
@@ -11,4 +11,30 @@ public:
     Hero(std::string _name, int _health, int _armor, int _attack) : name(_name), Character(_health, _armor, _attack) {}
     std::string getName() { return name; }
     std::string getType() override { return "Hero"; }
+
+    void Shop(Hero player)
+    {
+
+        int flag=0;
+
+        std::cout << "What fo you want to buy?" << std::endl;
+        std::cout << "For restore healt press 1 " << std::endl;
+        std::cout << "For upgrade attack point perss 2 " << std::endl;
+        std::cout << "For upgrade attack point perss 3" << std::endl;
+
+        std::cin >> flag;
+
+        if (flag = 1)
+        {
+            player.health = 100;
+        }
+        if (flag = 2)
+        {
+            player.attack_point++;
+        }
+        if (flag = 3)
+        {
+            player.armor++;
+        }
+    }
 };
